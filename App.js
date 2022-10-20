afff69a6f20ebe02ce6754f74d970a9854051b43
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2022-10-20 14:35:52 +0500

    Upgrades

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/views/screens/LoginScreen';
-import LoginWithScreen from './src/views/screens/LogInWithScreen';
-import RegistrationScreen from './src/views/screens/RegistrationScreen';
-import CountriesScreen from './src/views/screens/CountriesScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./src/views/screens/LoginScreen";
+import LoginWithScreen from "./src/views/screens/LogInWithScreen";
+import RegistrationScreen from "./src/views/screens/RegistrationScreen";
+import CountriesScreen from "./src/views/screens/CountriesScreen";
+// import { Text } from "react-native";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    // <Text>sdfjknsdf sdkfjnkf kjsdfk sdfkjnk </Text>
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name='LoginWithScreen' component={LoginWithScreen}/>
-        <Stack.Screen name='RegistrationScreen' component={RegistrationScreen} />
-        <Stack.Screen name='CountriesScreen' component={CountriesScreen} />
-        <Stack.Screen name='LoginScreen' component={LoginScreen} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="LoginWithScreen" component={LoginWithScreen} />
+        <Stack.Screen
+          name="RegistrationScreen"
+          component={RegistrationScreen}
+        />
+        <Stack.Screen name="CountriesScreen" component={CountriesScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ index.js
@@ -0,0 +1,52 @@
+// import React from "react";
+import { AppRegistry } from "react-native";
+
+// import { registerRootComponent } from "expo";
+
+import App from "./App";
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+// registerRootComponent(App);
+AppRegistry.registerComponent("main", () => App);
+
+// import React from "react";
+// import { AppRegistry, StyleSheet, Text, View } from "react-native";
+
+// const RNHighScores = () => {
+//   //   const contents = scores.map((score) => (
+//   //     <Text key={score.name}>
+//   //       {score.name}:{score.value}
+//   //       {"\n"}
+//   //     </Text>
+//   //   ));
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.highScoresTitle}>2048 High Scores!</Text>
+//       {/* <Text style={styles.scores}>{contents}</Text> */}
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#FFFFFF",
+//   },
+//   highScoresTitle: {
+//     fontSize: 20,
+//     textAlign: "center",
+//     margin: 10,
+//   },
+//   scores: {
+//     textAlign: "center",
+//     color: "#333333",
+//     marginBottom: 5,
+//   },
+// });
+
+// // Module name
+// AppRegistry.registerComponent("main", () => RNHighScores);
